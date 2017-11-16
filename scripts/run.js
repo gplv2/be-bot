9da8578d8d24097be96fd706906be022425f0b15
Glenn Plas <glenn@byte-consult.be>
2017-11-16 20:33:00 +0000

    static streetmap idea

--- scripts/run.js
+++ scripts/run.js
@@ -2,8 +2,8 @@
 // Runs a command on hubot
 // 
 // Commands:
-// hubot streets <postcode> [date] - runs a stats on hubot host
-// hubot last  <imei> - runs a lastseen against the api
+// hubot streets <postcode> [partialstring] - searches for streetlist in postcode
+// hubot stats  <imei> - runs a lastseen against the api
 
 // showrec /var/lib/ev_data/all/2015/10/06/Event_357460035633564
 // showstats /var/lib/ev_data/all/2015/10/06/Event_357460035633564
@@ -18,10 +18,65 @@ function pad(n, width, z) {
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
 
-var sdk = require("matrix-js-sdk");
+
+// var sdk = require("matrix-js-sdk");
 // var client = sdk.createClient("https://matrix.org");
 
 module.exports = function(robot) {
+	console.log(robot);
+        robot.respond("/streetmap ([0-9]+)(?: (.*))?$/i", function(msg) {
+		// Call static map generator http://staticmap.openstreetmap.de/staticmap.php?center=43.714728,5.998672&zoom=14&size=865x512&maptype=mapnik
+		// markers=40.702147,-74.015794,lightblue1|40.711614,-74.012318,lightblue2|40.718217,-73.998284,lightblue3
+		// maptype=cycle
+		//
+		//msg.send("ACK for effort: "+msg.match);
+		//console.log(msg.envelope.message.rawMessage.channel);
+		//console.log(msg.match);
+
+		var d = new Date();
+		var cmd ="";
+
+		// return true;
+		if(msg.match[1]) {
+			cmd = "building static map from street in postcode " +  msg.match[1];
+			if(msg.match[2]) {
+				url = "http://grbtiles.byteless.net/streets/?limit=10&postcode=" + msg.match[1] + "&meta=map&search=" + msg.match[2] ;
+			} else {
+				url = "http://grbtiles.byteless.net/streets/?limit=10&postcode=" + msg.match[1] + "&meta=map";
+			}
+
+			request({
+				url: url,
+				json: true
+			},function(error,response,body){
+				if (!error && response.statusCode === 200) {
+					console.log(body); // Print the json response
+					//var sdk = require("matrix-js-sdk");
+					//var client = sdk.createClient("https://matrix.org");
+					msg.send(JSON.stringify(cmd));
+
+					var obj = body;
+					console.log(obj);
+
+					/*
+					var reply = "\n" + msg.match[1] + " street count: ";
+					
+					Object.keys(obj).forEach(function(key, idx) {
+						//console.log(key + ": " + obj[key]);
+						reply=reply + obj[key]['street_total'] + '\n';
+					});
+					*/
+					// http://staticmap.openstreetmap.de/staticmap.php?center=43.714728,5.998672&zoom=14&size=865x512&maptype=mapnik
+					var smap = "http://staticmap.openstreetmap.de/staticmap.php?center="+ obj.b + "," + obj.l + "&zoom=14&size=865x512&maptype=mapnik";
+
+					msg.reply(smap);
+				}
+			});
+
+			return false;
+		}
+	});
+
 	robot.respond("/stats ([0-9]+)$/i", function(msg) {
 		//msg.send("ACK for effort: "+msg.match);
 		//console.log(msg.envelope.message.rawMessage.channel);
@@ -61,7 +116,6 @@ module.exports = function(robot) {
 		}
 	});
 
-	// robot.respond("/streets (.*)$/i", function(msg) 
         robot.respond("/streets ([0-9]+)(?: (.*))?$/i", function(msg) {
         	//robot.respond("/streets ([0-9]+)(?: (.*))$/i", function(msg) 
 		//console.log(msg.envelope.message.rawMessage.channel);
@@ -104,7 +158,7 @@ module.exports = function(robot) {
 
 					Object.keys(obj).forEach(function(key, idx) {
 						//console.log(key + ": " + obj[key]);
-						reply=reply + obj[key]['sname'] + " (" + obj[key]['dataurl'] + ' )\n';
+						reply=reply + obj[key]['sname'] + ' ( ' + obj[key]['dataurl'] + ' )\n';
 					});
 
 					msg.reply(reply);
