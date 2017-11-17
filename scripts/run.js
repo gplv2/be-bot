b718f273ebacba79019d34ba5988958f834cd1b3
Glenn Plas <glenn@byte-consult.be>
2017-11-17 13:09:01 +0000

    small fix

--- scripts/run.js
+++ scripts/run.js
@@ -1,16 +1,21 @@
 // Commands:
+// hubot active [now|today] - shows the active 10 mappers of the moment or by total of the day
 // hubot streets <postcode> [partialstring] - searches for streetlist in postcode
 // hubot streetmap <postcode> [partialstring] [cycle|mapnik] - creates a static image given by the bounds of the street we search
 // hubot stats <postcode> - Shows you a count for the number of streets in that postcode
+// hubot changeset <number> - Blame someone
 //
-// hubot author Glenn Plas for OSM.BE
-
+// author Glenn Plas for OSM.BE
 
 
 var request = require('request');
 var slackBot = require('hubot-matrix');
 // So we can calculate things from source API's
 var geolib = require('geolib');
+var parseString = require('xml2js').parseString;{
+
+var Memcached = require('memcached');
+var memcached = new Memcached('127.0.0.1:11211');
 
 function pad(n, width, z) {
   z = z || '0';
@@ -24,11 +29,88 @@ function pad(n, width, z) {
 
 module.exports = function(robot) {
     console.log(robot);
-    robot.respond("/streetmap ([0-9]+)(?: (.*))?$/i", function(msg) {
+
+    robot.respond(/who is @?([\w .\-]+)\?*$/i, function(res, done) {
+        name = res.match[1].trim();
+        users = robot.brain.usersForFuzzyName(name);
+
+        if(users.length >= 1) {
+            var user = users[0]
+            // Do something interesting here.. 
+            res.send(name + " is user " + user, done);
+        }
+    });
+
+    robot.respond("/active(?: (.*))?$/i", function(msg) {
+        var xml = memcached.get( "changesets", function( err, result ){
+            console.dir( result );
+            //xml.push(result);
+        });
+
+        var url = "http://api.openstreetmap.org/api/0.6/changesets?bbox=2.52,50.64,5.94,51.51";
+        var obj = {};
+
+        if (!xml.length) {
+            request({
+                url: url,
+                json: true
+            },function(error,response,body){
+                if (!error && response.statusCode === 200) {
+                    //console.log(body); // Print the json response
+                    //var sdk = require("matrix-js-sdk");
+                    //var client = sdk.createClient("https://matrix.org");
+                    parseString(body, function (err, result) {
+                        // console.log(result);
+                        if (err) {
+                            msg.reply("API reply: " +err);
+                            return false;
+                        }
+
+                        //console.log(result);
+                        msg.send('Loaded changeset data');
+                        obj=result['osm']['changeset'];
+                        //console.log(obj);
+                        //
+                        memcached.set( "changesets", obj , 300, function( err, result ) {
+                            if ( err ) {
+                                 console.error( err );
+                            }
+                       });
+                    });
+                } else {
+                    cmd = "Whoops! Houston, we have a problem: " + JSON.stringify(error);
+                    msg.reply(cmd);
+                }
+            });
+        }
+        console.log(obj);
+
+        var uarray = {};
+        // var sodasHad = robot.brain.get('totalSodas') * 1 || 0;
+        Object.keys(obj).forEach(function(key, idx) {
+            // console.log(obj[key]);
+            if(uarray[obj[key]['$']['uid']]) {
+                uarray[obj[key]['$']['uid']]=0;
+            } else {
+                uarray[obj[key]['$']['uid']]++;
+            }
+            // console.log(key + ": " + idx);
+            //reply=reply + obj[key]['street_total'] + '\n';
+            //reply=reply + obj[key]['street_total'] + '\n';
+        });
+        console.log(uarray);
+
+        msg.reply('OK');
+    });
+
+    robot.respond("/changeset ([0-9]+)(?: (.*))?$/i", function(msg) {
+        // BBOX changeset // Belgum : 2.52, 50.64, 5.94, 51.51
+        // http://api.openstreetmap.org/api/0.6/changesets?bbox=2.52,50.64,5.94,51.51&output=json
+        //
         // Call static map generator http://staticmap.openstreetmap.de/staticmap.php?center=43.714728,5.998672&zoom=14&size=865x512&maptype=mapnik
         // markers=40.702147,-74.015794,lightblue1|40.711614,-74.012318,lightblue2|40.718217,-73.998284,lightblue3
         // maptype=cycle
-        //
+
         //msg.send("ACK for effort: "+msg.match);
         //console.log(msg.envelope.message.rawMessage.channel);
         //console.log(msg.match);
@@ -77,13 +159,101 @@ module.exports = function(robot) {
                     }
 
                     /*
-                    var reply = "\n" + msg.match[1] + " street count: ";
+            var reply = "\n" + msg.match[1] + " street count: ";
 
-                    Object.keys(obj).forEach(function(key, idx) {
+            Object.keys(obj).forEach(function(key, idx) {
                     //console.log(key + ": " + obj[key]);
-                        reply=reply + obj[key]['street_total'] + '\n';
-                    });
-                    */
+            reply=reply + obj[key]['street_total'] + '\n';
+            });
+            */
+
+                    // Calculate the center of both values from crab api
+                    if (obj.b && obj.t && obj.r && obj.l ) {
+                        // http://staticmap.openstreetmap.de/staticmap.php?center=43.714728,5.998672&zoom=14&size=865x512&maptype=mapnik
+                        cmd = "building static map from " + obj.name + " for postcode " +  msg.match[1];
+                        msg.send(cmd);
+
+                        var center =geolib.getCenterOfBounds([
+                            {latitude: obj.b, longitude: obj.l},
+                            {latitude: obj.t, longitude: obj.r},
+                        ]);
+                        //  returns {"latitude": centerLat, "longitude": centerLng}
+                        var smap = "http://staticmap.openstreetmap.de/staticmap.php?center="+ center.latitude + "," + center.longitude + "&zoom=17&size=1865x1512&maptype=" + maptype;
+
+                        msg.reply(smap);
+                    } else {
+                        cmd = "Whoops! CRAB street object source has undefined coordinates!" + JSON.stringify(error);
+                        msg.reply(cmd);
+                    }
+                } else {
+                    cmd = "Whoops! Houston, we have a problem: " + JSON.stringify(error);
+                    msg.reply(cmd);
+                }
+            });
+            return false;
+        }
+    });
+
+    robot.respond("/streetmap ([0-9]+)(?: (.*))?$/i", function(msg) {
+        // Call static map generator http://staticmap.openstreetmap.de/staticmap.php?center=43.714728,5.998672&zoom=14&size=865x512&maptype=mapnik
+        // markers=40.702147,-74.015794,lightblue1|40.711614,-74.012318,lightblue2|40.718217,-73.998284,lightblue3
+        // maptype=cycle
+
+        //msg.send("ACK for effort: "+msg.match);
+        //console.log(msg.envelope.message.rawMessage.channel);
+        //console.log(msg.match);
+
+        var cmd ="";
+        var d = new Date();
+        var maptype="mapnik";
+
+        var searchstring="";
+        // Pick off last stuff / space separated
+        if(msg.match[2]) {
+            var myString = msg.match[2]
+            var splits = myString.split(' ', 2);
+            if(splits[1]=="cycle") {
+                maptype="cycle";
+                searchstring=splits[0];
+            } else if(splits[1]=="mapnik") {
+                maptype="mapnik";
+                searchstring=splits[0];
+            } else {
+                searchstring=myString;
+            }
+        }
+
+        if(msg.match[1]) {
+            if(msg.match[2]) {
+                url = "http://grbtiles.byteless.net/streets/?limit=10&postcode=" + msg.match[1] + "&meta=map&search=" + searchstring;
+            } else {
+                url = "http://grbtiles.byteless.net/streets/?limit=10&postcode=" + msg.match[1] + "&meta=map";
+            }
+
+            request({
+                url: url,
+                json: true
+            },function(error,response,body){
+                if (!error && response.statusCode === 200) {
+                    //console.log(body); // Print the json response
+                    //var sdk = require("matrix-js-sdk");
+                    //var client = sdk.createClient("https://matrix.org");
+                    var obj = body;
+                    console.log(obj);
+
+                    if (obj.error) {
+                        msg.reply("API reply: " +obj.error);
+                        return false;
+                    }
+
+                    /*
+            var reply = "\n" + msg.match[1] + " street count: ";
+
+            Object.keys(obj).forEach(function(key, idx) {
+                    //console.log(key + ": " + obj[key]);
+            reply=reply + obj[key]['street_total'] + '\n';
+            });
+            */
 
                     // Calculate the center of both values from crab api
                     if (obj.b && obj.t && obj.r && obj.l ) {
@@ -152,7 +322,6 @@ module.exports = function(robot) {
     });
 
     robot.respond("/streets ([0-9]+)(?: (.*))?$/i", function(msg) {
-        //robot.respond("/streets ([0-9]+)(?: (.*))$/i", function(msg) 
         //console.log(msg.envelope.message.rawMessage.channel);
         //console.log(msg);
         var d = new Date();
@@ -199,47 +368,47 @@ module.exports = function(robot) {
                     msg.reply(reply);
 
                     /*
-                    slackBot.send({
+            slackBot.send({
                     //text: 'Put the bunny back in the box.',
-                        text: '\"I\'m Castor Troy!\" - Face/Off (1997)',
+            text: '\"I\'m Castor Troy!\" - Face/Off (1997)',
                     //channel: msg.envelope.message.rawMessage.channel,
-                        username: 'Nicolas Cage',
-                        icon_emoji: ':facepunch:',
-                        attachments: [
-                            {
-                                "fallback": "Results of " + cmd,
+            username: 'Nicolas Cage',
+            icon_emoji: ':facepunch:',
+            attachments: [
+                {
+                "fallback": "Results of " + cmd,
 
-                                "color": "#36a64f",
+                "color": "#36a64f",
 
-                                "pretext": "You requested streets ..",
+                "pretext": "You requested streets ..",
 
-                                "author_name": msg.match[1],
-                                "author_link": "http://flickr.com/bobby/",
-                                "author_icon": "http://flickr.com/icons/bobby.jpg",
+                "author_name": msg.match[1],
+                "author_link": "http://flickr.com/bobby/",
+                "author_icon": "http://flickr.com/icons/bobby.jpg",
 
-                                "title": "OSM search " + msg.match[1],
-                                "title_link": "https://trac.synctrace.com/frontend/search?q=" + msg.match[1],
+                "title": "OSM search " + msg.match[1],
+                "title_link": "https://trac.synctrace.com/frontend/search?q=" + msg.match[1],
 
-                                "text": '```'+body+'```',
+                "text": '```'+body+'```',
 
-                                "mrkdwn_in": ["text", "pretext"],
+                "mrkdwn_in": ["text", "pretext"],
 
-                                "fields": [
-                                    {
-                                        "title": "Priority",
-                                        "value": "High",
-                                        "short": false
-                                    }
-                                ]
+                "fields": [
+                    {
+                    "title": "Priority",
+                    "value": "High",
+                    "short": false
+                    }
+                ]
                     //,
                     //"image_url": "http://my-website.com/path/to/image.jpg",
                     //"thumb_url": "http://example.com/path/to/thumb.png"
-                            }
-                        ]
+                }
+            ]
                     // OR 
                     //icon_url: 'http://www.internetmonk.com/wp-content/uploads/53513.gif'
-                    }); 
-                    */
+            }); 
+            */
                 } else {
                     msg.send(error);
                     msg.send(body);
@@ -265,4 +434,5 @@ module.exports = function(robot) {
     });
     */
 
+    memcached.end(); 
 };
